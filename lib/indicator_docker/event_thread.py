bbad0d0c4bf5766f456fbb2ba3bb05f6caa6f4e3
Dmitry Kann <yktooo@gmail.com>
2017-03-25 20:56:27 +0100

    Added live updates of containers

--- lib/indicator_docker/event_thread.py
+++ lib/indicator_docker/event_thread.py
@@ -5,17 +5,21 @@ import threading
 class EventThread(threading.Thread):
     """Threaded event handler implementation for dealing with Docker daemon events."""
 
-    def __init__(self, docker_client):
+    def __init__(self, docker_client, on_event):
         # We're a daemon thread (existence of which doesn't have to keep the whole program alive)
         super().__init__(daemon=True)
-        self.terminated = False
+        self.terminated    = False
         self.docker_client = docker_client
+        self.on_event      = on_event
 
     def run(self):
-        logging.debug("EventThread started, name `%s`",  self.name)
+        logging.debug('EventThread started, name `%s`',  self.name)
 
         # Loop over Docker events until terminated
-        for event in self.docker_client.events():
+        for event in self.docker_client.events(decode=True):
             if self.terminated:
                 break
-            logging.debug("Docker event: %s", str(event))
+            logging.debug('Docker event: %s', str(event))
+
+            # Call the callback
+            self.on_event(event)
